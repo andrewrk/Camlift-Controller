869cb0dc4c10cc623c44a4f44aba9a9ff3d4b2b3
Josh Wolfe <thejoshwolfe@gmail.com>
2009-08-18 11:41:11 +0000

    progress on Save autorun settings. also updated version number

---
+++ slnCamliftController/src/CamliftController/frmAutoRunSetups.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAutoRunSetups
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel
+        Me.btnDelete = New System.Windows.Forms.Button
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel
+        Me.btnOk = New System.Windows.Forms.Button
+        Me.btnCancel = New System.Windows.Forms.Button
+        Me.FlowLayoutPanel2 = New System.Windows.Forms.FlowLayoutPanel
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txtName = New System.Windows.Forms.TextBox
+        Me.lstSetups = New System.Windows.Forms.ListBox
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.FlowLayoutPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel2.AutoSize = True
+        Me.TableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.TableLayoutPanel2.ColumnCount = 1
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.btnDelete, 0, 0)
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(12, 185)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(65, 29)
+        Me.TableLayoutPanel2.TabIndex = 10
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(3, 3)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(59, 23)
+        Me.btnDelete.TabIndex = 5
+        Me.btnDelete.Text = "&Delete"
+        Me.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.btnOk, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnCancel, 1, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(201, 185)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(146, 29)
+        Me.TableLayoutPanel1.TabIndex = 8
+        '
+        'btnOk
+        '
+        Me.btnOk.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.btnOk.Location = New System.Drawing.Point(3, 3)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(67, 23)
+        Me.btnOk.TabIndex = 5
+        Me.btnOk.Text = "&OK"
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Location = New System.Drawing.Point(76, 3)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(67, 23)
+        Me.btnCancel.TabIndex = 6
+        Me.btnCancel.Text = "&Cancel"
+        '
+        'FlowLayoutPanel2
+        '
+        Me.FlowLayoutPanel2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlowLayoutPanel2.AutoSize = True
+        Me.FlowLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.FlowLayoutPanel2.Controls.Add(Me.Label2)
+        Me.FlowLayoutPanel2.Controls.Add(Me.txtName)
+        Me.FlowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.FlowLayoutPanel2.Location = New System.Drawing.Point(201, 12)
+        Me.FlowLayoutPanel2.Name = "FlowLayoutPanel2"
+        Me.FlowLayoutPanel2.Size = New System.Drawing.Size(146, 39)
+        Me.FlowLayoutPanel2.TabIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Name:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(3, 16)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(140, 20)
+        Me.txtName.TabIndex = 2
+        '
+        'lstSetups
+        '
+        Me.lstSetups.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lstSetups.FormattingEnabled = True
+        Me.lstSetups.IntegralHeight = False
+        Me.lstSetups.Location = New System.Drawing.Point(12, 12)
+        Me.lstSetups.Name = "lstSetups"
+        Me.lstSetups.Size = New System.Drawing.Size(182, 167)
+        Me.lstSetups.Sorted = True
+        Me.lstSetups.TabIndex = 12
+        '
+        'frmAutoRunSetups
+        '
+        Me.AcceptButton = Me.btnOk
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnCancel
+        Me.ClientSize = New System.Drawing.Size(359, 226)
+        Me.Controls.Add(Me.lstSetups)
+        Me.Controls.Add(Me.FlowLayoutPanel2)
+        Me.Controls.Add(Me.TableLayoutPanel2)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.MinimumSize = New System.Drawing.Size(314, 132)
+        Me.Name = "frmAutoRunSetups"
+        Me.Text = "frmAutoRunSetups"
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel2.ResumeLayout(False)
+        Me.FlowLayoutPanel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TableLayoutPanel2 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents btnOk As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents FlowLayoutPanel2 As System.Windows.Forms.FlowLayoutPanel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents lstSetups As System.Windows.Forms.ListBox
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+End Class
---
+++ slnCamliftController/src/CamliftController/frmAutoRunSetups.vb
@@ -0,0 +1,92 @@
+﻿Imports VisionaryDigital.SmartSteps
+Imports VisionaryDigital.Settings
+
+Public Class frmAutoRunSetups
+
+    Private m_smartStepsManager As SmartStepsManager
+    Private m_setups As List(Of KeyValuePair(Of String, AutorunSetupSettings))
+    Private m_saveMode As Boolean
+    Private m_selectedSelectedIndex As Integer = -1
+
+    Public Sub New(ByVal smartStepsManager As SmartStepsManager, ByVal saveMode As Boolean)
+        InitializeComponent() ' This call is required by the Windows Form Designer.
+
+        m_smartStepsManager = smartStepsManager
+        m_setups = smartStepsManager.AutorunSetups.AutorunSetups.ToList
+        m_saveMode = saveMode
+    End Sub
+
+    Private Sub frmAutoRunSetups_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        updatedSelection()
+    End Sub
+
+    Private Sub lstSetups_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lstSetups.SelectedIndexChanged
+        m_selectedSelectedIndex = findNameInSetups(lstSetups.SelectedItem)
+        updatedSelection()
+    End Sub
+
+    Private Sub btnDelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDelete.Click
+        lstSetups.Items.RemoveAt(lstSetups.SelectedIndex)
+        updatedSelection()
+    End Sub
+
+    Private Sub txtName_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtName.GotFocus
+        lstSetups.Sorted = False
+    End Sub
+
+    Private Sub txtName_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtName.LostFocus
+        lstSetups.Sorted = True
+    End Sub
+
+    Private Sub txtName_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtName.TextChanged
+        Dim currentEntry = m_setups(m_selectedSelectedIndex)
+        m_setups(m_selectedSelectedIndex) = New KeyValuePair(Of String, AutorunSetupSettings)(txtName.Text, currentEntry.Value)
+        fillList()
+    End Sub
+
+    Private Sub fillList()
+        Dim duplicateName = False 'TODO: add duplicate name detection
+        lstSetups.Enabled = Not duplicateName
+        btnOk.Enabled = Not duplicateName
+        If duplicateName Then Return
+
+        lstSetups.Items.Clear()
+        lstSetups.Items.AddRange(getNames())
+        lstSetups.SelectedItem = m_setups(m_selectedSelectedIndex).Key
+    End Sub
+    Private Function findNameInSetups(ByVal name As String) As Integer
+        Dim i = 0
+        For Each kvp In m_setups
+            If kvp.Key = name Then Return i
+            i += 1
+        Next
+        Return -1
+    End Function
+    Private Function getNames() As String()
+        Return (From kvp In m_setups Select kvp.Key).ToArray
+    End Function
+
+    Private Sub updatedSelection()
+        If lstSetups.SelectedItems.Count = 0 Then
+            btnDelete.Enabled = False
+            txtName.Text = ""
+            txtName.Enabled = False
+        Else
+            btnDelete.Enabled = True
+            txtName.Text = lstSetups.SelectedItem
+            txtName.Enabled = True
+        End If
+    End Sub
+
+    Private Sub btnOk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOk.Click
+        applySettings()
+        DialogResult = DialogResult.OK
+    End Sub
+    Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancel.Click
+        DialogResult = DialogResult.Cancel
+    End Sub
+
+    Private Sub applySettings()
+        m_smartStepsManager.AutorunSetups.AutorunSetups = m_setups.ToList
+    End Sub
+End Class
