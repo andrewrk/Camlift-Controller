ecab005ef43c7ade8b26b9596ec78654bec3ea8e
Josh Wolfe <thejoshwolfe@gmail.com>
2009-11-04 10:13:39 +0000

    release 2.1.6.201

--- slnCamliftController/My Project/AssemblyInfo.vb
+++ slnCamliftController/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.1.5.198")> 
-<Assembly: AssemblyFileVersion("2.1.5.198")> 
+<Assembly: AssemblyVersion("2.1.6.201")> 
+<Assembly: AssemblyFileVersion("2.1.6.201")> 
